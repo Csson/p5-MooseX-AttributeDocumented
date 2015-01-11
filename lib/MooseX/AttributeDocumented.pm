e641bacef8204b174e5f817c8c7758252674de6e
Erik Carlsson <info@code301.com>
2015-01-11 23:52:49 +0100

    Init

---
+++ lib/MooseX/AttributeDocumented.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package MooseX::AttributeDocumented;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use MooseX::AttributeDocumented;
+
+=head1 DESCRIPTION
+
+MooseX::AttributeDocumented is ...
+
+=head1 SEE ALSO
+
+=cut
